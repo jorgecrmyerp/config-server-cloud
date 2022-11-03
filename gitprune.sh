678032f8404e19792fc6b3d6c6d34d433bfb583f
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-03 17:43:24 +0100

    ju.,  3 de nov. de 2022 17:43:212022-11-03 ++falla cliente eureka

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-03 ++cliente eureka"
+message="$(date)2022-11-03 ++falla cliente eureka"
 fi
 
 start=`date +%s`
