c38d69b8792fda06b6c9b2366ee71455a4b0d4f2
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-04 17:29:24 +0100

    vi.,  4 de nov. de 2022 17:29:242022-11-04 microservicio yoda resiliencia

--- basico.sh
+++ basico.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-04 microservicio failover"
+message="$(date)2022-11-04 microservicio yoda resiliencia"
 fi
 
 git add .
