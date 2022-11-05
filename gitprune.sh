0bb1afbde9a25ca3b3e9e3d0f0390d1f4717442f
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-05 16:45:14 +0100

    sá.,  5 de nov. de 2022 16:45:102022-11-05 COMIENZO METRICAS

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-05 ACABADO ZIPKIN"
+message="$(date)2022-11-05 COMIENZO METRICAS"
 fi
 
 start=`date +%s`
