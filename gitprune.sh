1587e4052a35f73a8f78f80e89254e3e529f9525
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-07 10:23:33 +0100

    lu.,  7 de nov. de 2022 10:23:292022-11-07 ++ modificado puerto gateway para que no coincida con PROMETHEUS

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-07 PROMETHEUS"
+message="$(date)2022-11-07 ++ modificado puerto gateway para que no coincida con PROMETHEUS"
 fi
 
 start=`date +%s`
